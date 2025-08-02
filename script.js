cfe9499cb4ad30dbc6f7f7e3743083065472d2e6
VishalSaini7272 <sainivishal156@gmail.com>
2025-08-02 16:15:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let i = 0; i < str.length; i++) {
+    let ch = str[i];
+    if (str.indexOf(ch) === str.lastIndexOf(ch)) {
+      return ch;
+    }
+  }
+  return null;
+}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
